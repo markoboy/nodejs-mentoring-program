6f5a156ba88644ac694fb595a83e8f6268681b8b
Athanasios Markou <thanasismarko@gmail.com>
2021-10-01 17:20:30 +0100

    Extracted question prompt to a class

--- src/task1.1/index.js
+++ src/task1.1/index.js
@@ -2,29 +2,9 @@
  * Write a program which reads a string from the standard input stdin, reverses it
  * and then writes it to the standard output stdout.
  */
-import readline from 'readline';
+import QuestionPrompt from './QuestionPrompt';
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-/**
- * A promisified version of the ReadLine question.
- *
- * @param {string} query The query to ask the user.
- * @returns {Promise<string>} Returns the answer of the user.
- */
-const question = (query) =>
-  new Promise((resolve, reject) =>
-    rl.question(query, (answer) => {
-      if (!answer) {
-        reject();
-      }
-
-      resolve(answer);
-    })
-  );
+const questionPrompt = new QuestionPrompt();
 
 /**
  * The main function to start the program.
@@ -32,7 +12,9 @@ const question = (query) =>
  */
 export async function main() {
   try {
-    const answer = await question('What do you want me to reverse?\n');
+    const answer = await questionPrompt.question(
+      'What do you want me to reverse?\n'
+    );
 
     console.log('\nYour reversed answer is:');
     console.log(`${answer.split('').reverse().join('')}\n`);
