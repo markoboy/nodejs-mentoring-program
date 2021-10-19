7dd952b0e206e4372171c13e984d4977b0fa5b2f
Athanasios Markou <thanasismarko@gmail.com>
2021-10-19 15:31:20 +0100

    Removed redudant code

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,55 @@
-import commandLineArgs, { OptionDefinition } from 'command-line-args';
+import { UserEntity } from '@api/users/entities';
+import { UserRepository, USER_REPOSITORY_MODEL } from '@api/users/repositories';
+import { HashService, UserService } from '@api/users/services';
+import { MemoryDatabase } from '@core/database';
 
-import { start } from './taskRunner';
+async function main(): Promise<void> {
+    const database = new MemoryDatabase();
+    database.createModel(USER_REPOSITORY_MODEL);
+    const userRepo = new UserRepository(database);
+    const hashService = new HashService();
+    const userService = new UserService(userRepo, hashService);
 
-/**
- * Allow to run the file with some command line arguments.
- * The only available argument is the `task` with an alias of `t`.
- * We can define which task we want to execute by defining the task name.
- * It defaults to start the 1.1 task.
- *
- * @example
- * node index.js -t 1.1
- * node index.js --task 1.1
- *
- * @type {import('command-line-args').OptionDefinition[]}
- */
-const optionDefinitions: OptionDefinition[] = [
-    {
-        alias: 't',
-        defaultValue: '1.1',
-        name: 'task',
-        type: String
-    }
-];
+    const user = await userService.create({
+        login: 'aa',
+        password: 'aaa1',
+        age: 4
+    });
 
-const options = commandLineArgs(optionDefinitions);
+    await userService.create({
+        login: 'abb',
+        password: 'aaa1',
+        age: 4
+    });
 
-const { task } = options;
+    await userService.create({
+        login: 'aaaa',
+        password: 'aaa1',
+        age: 4
+    });
 
-start(`task${task}`);
+    await userService.create({
+        login: 'bb',
+        password: 'aaa1',
+        age: 4
+    });
+
+    console.log('First user: \n', JSON.stringify(await userService.findById(user.id), null, 2));
+
+    const updatedUser = await userService.updateOne(user.id, {
+        age: 5
+    });
+    console.log('Updated user: \n', JSON.stringify(await userService.findById(updatedUser.id), null, 2));
+
+    const users = await userService.getAutoSuggestUsers('bb', 6);
+    console.log('Autosuggest users: \n', JSON.stringify(users, null, 2));
+
+    await userService.deleteOne(users[0].id);
+
+    console.log(
+        'Find one user: \n',
+        await database.getModel<UserEntity>(USER_REPOSITORY_MODEL).find({ isDeleted: true }, { limit: 1 })
+    );
+}
+
+main();
