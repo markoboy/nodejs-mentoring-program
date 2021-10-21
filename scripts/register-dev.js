1ba6a9364c8f0e31b7e188ab768c537e9e28813a
Athanasios Markou <thanasismarko@gmail.com>
2021-10-21 11:25:30 +0100

    Updated comments

--- scripts/register-dev.js
+++ scripts/register-dev.js
@@ -1,7 +1,7 @@
 const tsConfig = require('../tsconfig.json');
 const registerPaths = require('./register-paths');
 
-// The base url for production is the baseUrl
+// The base url for development is the baseUrl
 const baseUrl = tsConfig.compilerOptions.baseUrl;
 
 registerPaths(baseUrl);
